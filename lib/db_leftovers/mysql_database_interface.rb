6e611dc5a871fdfa19e2f60f18aa4501a59b6619
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2012-09-30 08:09:58 -0700

    started real work on mysql database interface

--- lib/db_leftovers/mysql_database_interface.rb
+++ lib/db_leftovers/mysql_database_interface.rb
@@ -3,7 +3,28 @@ module DBLeftovers
   class MysqlDatabaseInterface < GenericDatabaseInterface
 
     def lookup_all_indexes
-      raise "stub"
+      ret = {}
+      ActiveRecord::Base.connection.select_values("SHOW TABLES").each do |table_name|
+        indexes = {}
+        ActiveRecord::Base.connection.select_rows("SHOW INDEXES FROM #{table_name}").each do |_, non_unique, key_name, seq_in_index, column_name, collation, cardinality, sub_part, packed, has_nulls, index_type, comment|
+          unless key_name == 'PRIMARY'
+            # Combine rows for multi-column indexes
+            h = (indexes[key_name] ||= { unique: !non_unique, name: key_name, columns: {} })
+            h[:columns][seq_in_index.to_i] = column_name
+          end
+        end
+
+        indexes.each do |index_name, h|
+          ret[index_name] = Index.new(
+            table_name,
+            h[:columns].sort.map{|k, v| v},
+            unique: h[:unique],
+            name: h[:name]
+          )
+        end
+      end
+
+      return ret
     end
 
     def lookup_all_foreign_keys
